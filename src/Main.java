d568cebae9d49874bd83e9ce53978d0d29dd37a5
barbier78u <maxime2852@gmail.com>
2019-12-11 10:05:23 +0100

    save correction(Todo)

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,11 @@ import model.game.player.tactic.CrossTactic;
 import model.game.player.tactic.RandomTactic;
 import model.game.ship.Ship;
 
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Launching game ...");
         ShipShop shipShop = new ShipShop();
         shipShop.createGame(EraFactory.getModernEra(), new RandomTactic(), true);
@@ -16,5 +19,6 @@ public class Main {
         System.out.println("Changing tactic for IA ...");
         shipShop.setTactic(1, new CrossTactic());
         System.out.println("Tactic Changed for IA !");
+        shipShop.save(new File("save/testSave.ser"));
     }
 }

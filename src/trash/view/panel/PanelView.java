ed72799ce0f2f38d6893de6a1b799bcb9090ad9b
Sies Reynault <reynault.sies8@etu.univ-lorraine.fr>
2019-12-19 15:54:37 +0100

    Nettoyage

--- src/trash/view/panel/PanelView.java
+++
@@ -1,8 +0,0 @@
-package trash.view.panel;
-
-import java.util.Observer;
-import java.util.ResourceBundle;
-
-public interface PanelView extends Observer {
-    void initialize(ResourceBundle resources);
-}
